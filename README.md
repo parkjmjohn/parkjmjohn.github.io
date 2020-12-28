1956cab13f2f533e6c09fd7ac9fe844db5de0957
John Park <mia@Johns-MacBook-Pro-3.local>
2020-12-27 21:25:51 -0800

    add: react env setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # portfolio
+
+### Setup
+`npm install`
+
+### Build
+`npm run start`
+
+### Lint
+`npm run lint`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Welcome to my portfolio
+    </div>
+  );
+};
+
+export default App;
